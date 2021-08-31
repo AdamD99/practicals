30341a33ab43bc5981aab86faa7198052c1c4c41
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-08-31 13:53:41 +1000

    add hex_colours program

---
+++ practicals/prac_05/hex_colours.py
@@ -0,0 +1,10 @@
+
+HEX_COLOURS = {"AliceBlue": "#f0f8ff", "aquamarine1": "#7fffd4", "burlywood": "#deb887", "chartreuse1": "#7fff00", "chocolate": "#d2691e", "CornflowerBlue": "#6495ed", "DarkGoldenrod": "#b8860b", "DarkOrange": "#ff8c00", "DarkOrchid": "#9932cc", "DarkViolet": "#9400d3"}
+
+colour = input("Enter colour: ").lower()
+while colour != "":
+    if colour in HEX_COLOURS:
+        print(colour, "is", HEX_COLOURS[colour])
+    else:
+        print("Invalid colour")
+    colour = input("Enter colour: ").lower()

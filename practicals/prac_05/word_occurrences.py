d24fe8c855270d17727949abf04a03cafc810301
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-09-02 19:49:12 +1000

    add dynamic formatting to word occurrences

--- practicals/prac_05/word_occurrences.py
+++ practicals/prac_05/word_occurrences.py
@@ -7,5 +7,6 @@ for word in word_list:
 
 word_list = list(word_count.keys())
 word_list.sort()
+word_length = max(len(word_list) for word in word_list)
 for word in word_list:
-    print("{} : {}".format(word, word_count[word]))
+    print("{:{}} : {}".format(word, word_length, word_count[word]))

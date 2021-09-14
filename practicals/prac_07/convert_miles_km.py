c4216b8198e108c34ad48bd1385ef0c704f591c5
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-09-14 13:19:45 +1000

    add formatting to button(s) in miles to km program

--- practicals/prac_07/convert_miles_km.py
+++ practicals/prac_07/convert_miles_km.py
@@ -5,11 +5,12 @@ from kivy.properties import StringProperty
 
 CONVERSION = 1.609344
 
+
 class ConvertMilesKmApp(App):
     output_km = StringProperty()
 
     def build(self):
-        Window.size = (750, 300)
+        Window.size = (400, 200)
         self.title = "Convert Miles to Kilometers"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
@@ -20,7 +21,6 @@ class ConvertMilesKmApp(App):
             self.output_km = str(kilometers)
         except ValueError:
             self.output_km = '0.0'
-
 
     def handle_increment(self, value):
         try:

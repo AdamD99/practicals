9c8234be304ca8dc272751d7fbcce76573fcdd98
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-10-10 13:04:43 +1000

    Add more test cases to unreliable_car_test.py

--- practicals/prac_08/unreliable_car_test.py
+++ practicals/prac_08/unreliable_car_test.py
@@ -1,5 +1,8 @@
 from practicals.prac_08.unreliable_car import UnreliableCar
 
-Car = UnreliableCar(100, 'Yaris', 50)
-Car.drive(40)
-print(Car)
+good_car = UnreliableCar(100, 'Good Car', 95)
+bad_car = UnreliableCar(100, 'Bad Car', 15)
+print(f"Attempting to drive 40km, {good_car.name} drove {good_car.drive(40)}km")
+print(f"Attempting to drive 40km, {bad_car.name} drove {bad_car.drive(40)}km")
+print(good_car)
+print(bad_car)

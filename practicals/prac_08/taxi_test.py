55fded8a17d7834cb37690fa53891bd6c81c8232
AdamD99 <adam.domenighini@my.jcu.edu.au>
2021-10-10 13:36:03 +1000

    Add silver_service_taxi.py and silver_service_taxi_test.py programs, Reformat class argument order.

--- practicals/prac_08/taxi_test.py
+++ practicals/prac_08/taxi_test.py
@@ -1,6 +1,6 @@
 from practicals.prac_08.taxi import Taxi
 
-Prius = Taxi(100, 'Prius 1')
+Prius = Taxi('Prius 1', 100)
 Prius.drive(40)
 print(f"{Prius}, Current Fare: ${Prius.get_fare():,.2f}")
 Prius.start_fare()
